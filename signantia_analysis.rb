ee1b5098926428cc75d963b312354729ce6598ed
tiredpixel <tiredpixel@gmail.com>
2011-06-28 21:46:31 +0100

    Parse script options using optparse.

--- signantia_analysis.rb
+++ signantia_analysis.rb
@@ -1,19 +1,39 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+
 require_relative 'lib/signantia_analysis'
 
 puts "signantia_analysis"
 
+# read options                                                                 #
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: signantia_analysis.rb [options]"
+  
+  opts.on(
+    '-d',
+    "--database DATABASE",
+    "The database URI - e.g. sqlite:db/example.sqlite3"
+    ) do |database|
+    options[:database] = database
+  end
+end.parse!
+
+# options are available in options, remaining arguments in ARGV
+
 # set up database                                                              #
 
-DataMapper::Logger.new(
-  STDOUT,
-  :debug
-  )
+#DataMapper::Logger.new(
+#  STDOUT,
+#  :debug
+#  )
 
 DataMapper.setup(
   :default,
-  ENV['DATABASE_URL'] || 'sqlite:db/development.sqlite3'
+  options[:database] || 'sqlite:db/development.sqlite3'
   )
 
 DataMapper.finalize
